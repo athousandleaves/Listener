ee2066bf6e8d021e00d6d00d1e69438292bc4418
athousandleaves <superamare@gmail.com>
2017-07-18 16:30:23 -0500

    set up API request function

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ var songList = document.getElementById('songlist');
 var extra = "";
 var links = [];
 var searchIcon = document.getElementById('search');
+
+// API call function
+function APIcaller(event) {
+  if (event.type === 'click' || event.type === 'keypress' && event.which === 13) {
+      var artist = input.value;
+      var xhr = new XMLHttpRequest();
+      xhr.onload = pull;
+      xhr.onerror = error;
+      xhr.open('get', `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist=${encodeURIComponent(artist)}&limit=10&api_key=${YOUR-API-KEY}&format=json`);
+      xhr.send();
