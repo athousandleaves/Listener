ae9dd9bcefc1742ad97196c31e91cc47e2539e57
athousandleaves <superamare@gmail.com>
2017-07-18 16:24:17 -0500

    add initial variables to render search data

---
+++ app.js
@@ -0,0 +1,5 @@
+var input = document.getElementById('input');
+var songList = document.getElementById('songlist');
+var extra = "";
+var links = [];
+var searchIcon = document.getElementById('search');
