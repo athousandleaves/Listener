29523a61a28fca3bef86454ed3d000ffda546697
athousandleaves <superamare@gmail.com>
2017-10-01 17:35:02 -0500

    add playlist button

--- app.js
+++ app.js
@@ -79,9 +79,10 @@ function displayContent(songs) {
 
   songList.innerHTML = html;
   extra = document.querySelectorAll(".extra");
+  playlist = document.getElementById('playlist');
 }
 
-function youTube(search) {
+function youTube(search, index) {
   var xhr = new XMLHttpRequest();
   xhr.onload = ytListener;
   xhr.onerror = ytError;
@@ -94,7 +95,13 @@ function youTube(search) {
   function ytListener() {
     var parse = JSON.parse(this.responseText);
     var id = parse.items[0].id.videoId;
+    ytLinks.splice(index, 0, id);    
     extra.item(index).innerHTML += '<a class="ui mini basic green button" id="songlink" href="https://www.youtube.com/watch?v=' + id + '">Listen</a>';
+    // when all api requests have finished, print the playlist
+    if (ytLinks.length === 10) {
+      var trax = ytLinks.join(',');
+      playlist.innerHTML += "<a class='ui basic large button' id='playall' href='https://www.youtube.com/watch_videos?video_ids=" + trax + "'><i id='vplay' class='video play icon'></i> <strong>Play All</strong></a>";
+    }  
   }
 
   function ytError(err) {
